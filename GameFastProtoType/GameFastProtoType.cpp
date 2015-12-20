655eddda69470a7aaa088a94b1c2af807fb19bcb
funcoder3d <funcoder3d@gmail.com>
2015-12-20 13:52:53 +0900

    Add Ment

--- GameFastProtoType/GameFastProtoType.cpp
+++ GameFastProtoType/GameFastProtoType.cpp
@@ -6,7 +6,9 @@
 
 int _tmain(int argc, _TCHAR* argv[])
 {
-	printf("Hellow World - My ProtoType\n");
+	printf("Hellow World - My ProtoType1\n");
+	printf("Hellow World - My ProtoType2\n");
+	printf("Hellow World - My ProtoType3\n");
 	return 0;
 }
 
